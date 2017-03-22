641c1c17bd255d77334c6a577ede6210d077edda
PY131 <289683309@qq.com>
2017-03-22 11:28:03 +0800

    update the code of 3.5

--- ch3_linear_model/3.5_LDA/src/LDA.py
+++ ch3_linear_model/3.5_LDA/src/LDA.py
@@ -5,6 +5,7 @@ Create on 2017/3/21
 
 @author PY131
 '''''
+from _operator import inv
 
 '''
 data importion and pre-analysis
@@ -33,7 +34,7 @@ plt.legend(loc = 'upper right')
 '''
 LDA via sklearn
 '''
-
+'''
 from sklearn import model_selection
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn import metrics
@@ -43,7 +44,7 @@ import matplotlib.pyplot as plt
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.5, random_state=0)
 
 # model fitting
-lda_model = LinearDiscriminantAnalysis(solver='lsqr', shrinkage=None).fit(X, y)
+lda_model = LinearDiscriminantAnalysis(solver='lsqr', shrinkage=None).fit(X_train, y_train)
 
 # model validation
 y_pred = lda_model.predict(X_test)
@@ -55,10 +56,14 @@ print(metrics.classification_report(y_test, y_pred))
 # draw the classfier decision boundary
 f2 = plt.figure(2) 
 h = 0.001
-x0_min, x0_max = X[:, 0].min()-0.1, X[:, 0].max()+0.1
-x1_min, x1_max = X[:, 1].min()-0.1, X[:, 1].max()+0.1
-x0, x1 = np.meshgrid(np.arange(x0_min, x0_max, h),
-                     np.arange(x1_min, x1_max, h))
+# x0_min, x0_max = X[:, 0].min()-0.1, X[:, 0].max()+0.1
+# x1_min, x1_max = X[:, 1].min()-0.1, X[:, 1].max()+0.1
+
+x0, x1 = np.meshgrid(np.arange(-1, 1, h),
+                     np.arange(-1, 1, h))
+
+# x0, x1 = np.meshgrid(np.arange(x0_min, x0_max, h),
+#                      np.arange(x1_min, x1_max, h))
 
 z = lda_model.predict(np.c_[x0.ravel(), x1.ravel()]) 
 
@@ -72,8 +77,8 @@ plt.xlabel('density')
 plt.ylabel('ratio_sugar')  
 plt.scatter(X[y == 0,0], X[y == 0,1], marker = 'o', color = 'k', s=100, label = 'bad')
 plt.scatter(X[y == 1,0], X[y == 1,1], marker = 'o', color = 'g', s=100, label = 'good')
-plt.show()
-
+# plt.show()
+'''
 
 '''
 implementation of LDA based on self-coding
@@ -81,8 +86,24 @@ implementation of LDA based on self-coding
 
 # computing the d-dimensional mean vectors
 # import numpy as np
+# 1-st. get the mean vector of each class
+# u = []  
+# for i in range(2): # two class
+#     u.append(np.mean(X[y==i], axis=0))  # column mean
+# 
+# # 2-nd. computing the within-class scatter matrix, refer on book (3.33)
+# m,n = np.shape(X)
+# Sw = np.zeros((n,n))
+# for i in range(m):
+#     x_tmp = np.mat(X[i]).T
+#     if   y[i] == 0: u_tmp = np.mat(u[0]).T
+#     elif y[i] == 1: u_tmp = np.mat(u[1]).T
+#     Sw += (x_tmp - u_tmp) * (x_tmp - u_tmp).T
+# print(Sw)
+        
 
 # 1-st. get the mean vector of each class
+
 u = []  
 for i in range(2): # two class
     u.append(np.mean(X[y==i], axis=0))  # column mean
@@ -96,19 +117,26 @@ for i in range(m):
     if y[i] == 1: u_tmp = u[1].reshape(n,1)
     Sw += np.dot( x_tmp - u_tmp, (x_tmp - u_tmp).T )
 
+Sw = np.mat(Sw)
+U, sigma, V= np.linalg.svd(Sw) 
+
+Sw_inv = V.T * np.linalg.inv(np.diag(sigma)) * U.T
 # 3-th. computing the parameter w, refer on book (3.39)
-w = np.dot( Sw**-1, (u[0] - u[1]).reshape(n,1) )  # here we use a**-1 to get the inverse of a ndarray
+w = np.dot( Sw_inv, (u[0] - u[1]).reshape(n,1) )  # here we use a**-1 to get the inverse of a ndarray
 
 print(w)
 
 # 4-th draw the LDA line in scatter figure
 
-f2 = plt.figure(2)
-     
+# f2 = plt.figure(2)
+f3 = plt.figure(3)
+plt.xlim( -0.2, 1 )
+plt.ylim( -0.5, 0.7 )
+
 p0_x0 = -X[:, 0].max()
-p0_x1 = ( -w[0,0] / w[1,0] ) * p0_x0
+p0_x1 = ( w[1,0] / w[0,0] ) * p0_x0
 p1_x0 = X[:, 0].max()
-p1_x1 = ( -w[0,0] / w[1,0] ) * p1_x0
+p1_x1 = ( w[1,0] / w[0,0] ) * p1_x0
 
 plt.title('watermelon_3a - LDA')  
 plt.xlabel('density')  
@@ -124,7 +152,74 @@ from self_def import GetProjectivePoint_2D
 
 m,n = np.shape(X)
 for i in range(m):
-    x_p = GetProjectivePoint_2D( [X[i,0], X[i,1]], [-w[0,0] / w[1,0] , 0] ) 
+    x_p = GetProjectivePoint_2D( [X[i,0], X[i,1]], [w[1,0] / w[0,0] , 0] ) 
+    if y[i] == 0: 
+        plt.plot(x_p[0], x_p[1], 'ko', markersize = 5)
+    if y[i] == 1: 
+        plt.plot(x_p[0], x_p[1], 'go', markersize = 5)   
+    plt.plot([ x_p[0], X[i,0]], [x_p[1], X[i,1] ], 'c--', linewidth = 0.3)
+
+# plt.show()
+
+'''
+implementation of LDA again after delete outlier (X[14])
+'''
+# computing the d-dimensional mean vectors
+# import numpy as np
+
+# 1-st. get the mean vector of each class
+X = np.delete(X, 14, 0)
+y = np.delete(y, 14, 0)
+
+u = []  
+for i in range(2): # two class
+    u.append(np.mean(X[y==i], axis=0))  # column mean
+
+# 2-nd. computing the within-class scatter matrix, refer on book (3.33)
+m,n = np.shape(X)
+Sw = np.zeros((n,n))
+for i in range(m):
+    x_tmp = X[i].reshape(n,1)  # row -> cloumn vector
+    if y[i] == 0: u_tmp = u[0].reshape(n,1)
+    if y[i] == 1: u_tmp = u[1].reshape(n,1)
+    Sw += np.dot( x_tmp - u_tmp, (x_tmp - u_tmp).T )
+
+Sw = np.mat(Sw)
+U, sigma, V= np.linalg.svd(Sw) 
+
+Sw_inv = V.T * np.linalg.inv(np.diag(sigma)) * U.T
+# 3-th. computing the parameter w, refer on book (3.39)
+w = np.dot( Sw_inv, (u[0] - u[1]).reshape(n,1) )  # here we use a**-1 to get the inverse of a ndarray
+
+print(w)
+
+# 4-th draw the LDA line in scatter figure
+
+# f2 = plt.figure(2)
+f4 = plt.figure(4)
+plt.xlim( -0.2, 1 )
+plt.ylim( -0.5, 0.7 )
+
+p0_x0 = -X[:, 0].max()
+p0_x1 = ( w[1,0] / w[0,0] ) * p0_x0
+p1_x0 = X[:, 0].max()
+p1_x1 = ( w[1,0] / w[0,0] ) * p1_x0
+
+plt.title('watermelon_3a - LDA')  
+plt.xlabel('density')  
+plt.ylabel('ratio_sugar')  
+plt.scatter(X[y == 0,0], X[y == 0,1], marker = 'o', color = 'k', s=10, label = 'bad')
+plt.scatter(X[y == 1,0], X[y == 1,1], marker = 'o', color = 'g', s=10, label = 'good')
+plt.legend(loc = 'upper right')  
+
+plt.plot([p0_x0, p1_x0], [p0_x1, p1_x1])
+
+# draw projective point on the line
+from self_def import GetProjectivePoint_2D 
+
+m,n = np.shape(X)
+for i in range(m):
+    x_p = GetProjectivePoint_2D( [X[i,0], X[i,1]], [w[1,0] / w[0,0] , 0] ) 
     if y[i] == 0: 
         plt.plot(x_p[0], x_p[1], 'ko', markersize = 5)
     if y[i] == 1: 
@@ -132,6 +227,4 @@ for i in range(m):
     plt.plot([ x_p[0], X[i,0]], [x_p[1], X[i,1] ], 'c--', linewidth = 0.3)
 
 plt.show()
-
-
 
